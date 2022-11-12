20b25fdc6d25a34df321e5ef1f43db1b9015b523
huanglongjian1 <527921342@qq.com>
2022-11-12 17:15:17 +0800

    git add

---
+++ app/src/main/java/com/example/hello1/NewClass1.java
@@ -0,0 +1,13 @@
+package com.example.hello1;
+
+public class NewClass1 {
+    double d;
+
+    public double getD() {
+        return d;
+    }
+
+    public void setD(double d) {
+        this.d = d;
+    }
+}
