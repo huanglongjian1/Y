04bbc9c972a7840c91aa2939d5eeff5bcc5734ac
huanglongjian1 <527921342@qq.com>
2022-11-12 17:25:18 +0800

    git add

---
+++ app/src/main/java/com/example/hello1/NewClass.java
@@ -0,0 +1,22 @@
+package com.example.hello1;
+
+public class NewClass {
+    int I;
+    String string;
+
+    public int getI() {
+        return I;
+    }
+
+    public void setI(int i) {
+        I = i;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+}
